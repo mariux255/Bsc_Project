99de8b1b6546b2f222c9af5668c4629024be0cfc
Marius Jonika <mjo@PHUMBPLOAN.local>
2020-08-01 19:58:49 +0200

    new approach to dowloading and cutting. New format. Seems to work good now.

--- extract.py
+++ extract.py
@@ -7,107 +7,110 @@ import youtube_dl
 import string
 import random
 from multiprocessing.dummy import Pool
+from urllib.parse import urlparse, parse_qs
 
 def download_yt_videos(indexfile):
     failed_list = []
-    prev_url = ""
-    prev_sign_name = ""
-    prev_file_name = ""
-    fail_count = 0
+    url_list = []
+    id_list = []
+
     content = json.load(open(indexfile))
-    saveto = "raw_videos"
+    saveto = "originals"
     if not os.path.exists(saveto):
         os.mkdir(saveto)
     
     for entry in content:
-        # random_character2 = random.choice(string.ascii_letters)
         video_url = entry['url']
+        url_list.append(video_url)
+        
+    url_list = list(set(url_list))
+
+
+    for url in url_list:
+        video_id = extract_video_id(url)
+        id_list.append(video_id)
+        if video_id == None:
+            failed_list.append(url)
+            continue
+        if not os.path.exists(saveto + "/" + video_id):
+            os.mkdir(saveto + "/" + video_id)
+        else:
+            continue
+        ydl_opts = {'outtmpl': '{}'.format(saveto + "/" + video_id + "/" + video_id)}
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            try:
+                info_dict = ydl.extract_info(url, download=False)
+                video_ext = info_dict.get("ext", None)
+
+                ydl.download([url])
+                # cmd = "youtube-dl \"{}\" -o \"{}%(id)s.%(ext)s\"".format(video_url, saveto + "/" + sign_name + os.path.sep)
+                # os.system(cmd)
+            except:
+                continue
+    for a_id in id_list:
+        for fname in os.listdir(saveto + "/" + a_id):
+            try:
+                if fname.endswith(".mp4"):
+                    continue
+                else:
+                    format_cmd = "ffmpeg -i {} {}".format(saveto + "/" + a_id + "/" + a_id +
+                                                  "." + "mkv", saveto + "/" + a_id + "/" + a_id + "." + "mp4")
+                    os.system(format_cmd)
+                    os.remove(saveto + "/" + a_id + "/" + a_id + "." + "mkv")
+            except:
+                continue
+    print(failed_list)
+
+def cut_videos(indexfile):
+    content = json.load(open(indexfile))
+    saveto = "cut_videos"
+    if not os.path.exists(saveto):
+        os.mkdir(saveto)
+    for entry in content:
+        video_url = entry['url']
+        video_id = extract_video_id(video_url)
+        og_directory = "originals"
         sign_name = entry['clean_text']
         sign_name = sign_name.replace(" ", "")
         file_name = sign_name
         start_time = entry['start_time']
         end_time = entry['end_time']
+        start_time = reformat_time(start_time)
+        end_time = reformat_time(end_time)
 
         if not os.path.exists(saveto + "/" + sign_name):
             os.mkdir(saveto + "/" + sign_name)
 
-        if prev_url == video_url:
-            # cut video
-            cut_videos(saveto, sign_name, start_time, end_time)
-            
-        else:
-            # download and cut video
-            try:
-                download_video(video_url,sign_name,file_name,saveto)
-            except:
-                continue
+        try:
+            _, _, files = next(os.walk("{}".format(saveto + "/" + sign_name + "/")))
+            file_count = len(files)
+        except:
+            print("MADNESS")
+            file_count = 100
 
-            try:
-                cut_videos(saveto,sign_name,start_time, end_time)
-            except:
-                failed_list.append(sign_name)
-                continue
-
-            try:
-                os.remove(saveto + "/" + prev_sign_name + "/" + prev_file_name + "." + "mp4")
-            except:
-                pass
-
-        prev_sign_name = sign_name
-        prev_file_name = sign_name
-        prev_url = video_url
-    print(failed_list)
+        new_file_name = sign_name + str(file_count)
 
 
-def download_video(video_url, sign_name, file_name, saveto):
-    ydl_opts = {'outtmpl': '{}'.format(
-        saveto + "/" + sign_name + "/" + file_name)}
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        
-        ydl.download([video_url])
-        info_dict = ydl.extract_info(video_url, download=False)
-        video_ext = info_dict.get("ext", None)
-        # cmd = "youtube-dl \"{}\" -o \"{}%(id)s.%(ext)s\"".format(video_url, saveto + "/" + sign_name + os.path.sep)
-        # os.system(cmd)
-        if video_ext == "webm":
-            format_cmd = "ffmpeg -i {} {}".format(saveto + "/" + sign_name + "/" + file_name +
-                                                  "." + "mkv", saveto + "/" + sign_name + "/" + file_name + "." + "mp4")
-            os.system(format_cmd)
-            os.remove(saveto + "/" + sign_name +
-                      "/" + file_name + "." + "mkv")
-            video_ext = "mp4"
+        ffm_cmd = "ffmpeg -ss {} -to {} -i {} -an -c copy {}".format(start_time, end_time, og_directory + "/" + video_id + "/" + video_id + "." + "mp4", saveto + "/" + sign_name + "/" + new_file_name + ".mp4")
+        os.system(ffm_cmd)
 
 
-def cut_videos(saveto, sign_name, start_time, end_time):
-    try:
-        _, _, files = next(os.walk("{}".format(saveto + "/" + sign_name + "/")))
-        file_count = len(files)
-    except:
-        print("MADNESS")
-        file_count = 100
-
-    file_name = sign_name
-    new_file_name = sign_name + str(file_count)
-
-    d_time = reformat_time((end_time - start_time))
-    start_time = reformat_time(start_time)
-    end_time = reformat_time(end_time)
-
-           
-
-    ffm_cmd = "ffmpeg -ss {} -t {} -i {}  -an -c copy {}".format(start_time, d_time, saveto + "/" + sign_name + "/" + file_name + "." + "mp4", saveto + "/" + sign_name + "/" + new_file_name + ".mp4")
-    os.system(ffm_cmd)
-    # os.remove(saveto + "/" + sign_name + "/" + file_name + "." + "mp4")
-
-
+def extract_video_id(url):
+    u_pars = urlparse(url)
+    quer_v = parse_qs(u_pars.query).get('v')
+    if quer_v:
+        return quer_v[0]
+    pth = u_pars.path.split('/')
+    if pth:
+        return pth[-1]
 
 def reformat_time(time):
     if int(time)>=10 and int(time) < 60:
         time = "00:00:{}".format(time)
     elif int(time)>=60 :
-        hours = time // 3600
+        hours = int(time // 3600)
         time = time - hours * 3600
-        minutes = time // 60
+        minutes = int(time // 60)
         seconds = time - minutes * 60
         if minutes >= 10:
             time ="0{}:{}:{}".format(hours,minutes,seconds)
@@ -117,6 +120,7 @@ def reformat_time(time):
         time = "00:00:0{}".format(time)
     return time
 
-
 if __name__ == '__main__':
     download_yt_videos('extraction_test.json')
+    cut_videos('extraction_test.json')
+            
